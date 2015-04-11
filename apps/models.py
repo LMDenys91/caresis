f4f299ded23e22fad67be6db25ff10f31bdcb52e
Lucas Denys <lmd238@cornell.edu>
2015-04-11 18:16:19 -0400

    new uncoupled model

--- apps/models.py
+++ apps/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django import forms
 
-class User(models.Model):
+class Patient(models.Model):
 	id = models.AutoField(primary_key=True)
 	firstName = models.CharField(max_length=200)
 	lastName = models.CharField(max_length=200)
@@ -9,16 +9,27 @@ class User(models.Model):
 	phone = forms.RegexField(regex=r'^\+?1?\d{9,15}$', 
                                 error_message = ("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
 	uber_token = models.CharField(max_length=200)
-	primary_advocate = models.ForeignKey('self',related_name="advocate")
-	patient = models.ForeignKey('self', related_name="patients")
 
 	def __str__(self):
         	return self.firstName
 
+class Advocate(models.Model):
+	id = models.AutoField(primary_key=True)
+	firstName = models.CharField(max_length=200)
+	lastName = models.CharField(max_length=200)
+	address = models.CharField(max_length=200)
+	phone = forms.RegexField(regex=r'^\+?1?\d{9,15}$', 
+                                error_message = ("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
+	uber_token = models.CharField(max_length=200)
+	patient = models.ForeignKey(Patient)
+
+	def __str__(self):
+        	return self.firstName
+
+
 class Appointment(models.Model):
  	id = models.AutoField(primary_key=True)
- 	patient = models.ForeignKey(User, related_name="patient")
-	primary_advocate = models.ForeignKey(User, "advocate")
+	advocate = models.ForeignKey(Advocate)
  	title = models.CharField(max_length=200)
  	description = models.CharField(max_length=200)
  	latitude = models.FloatField()
