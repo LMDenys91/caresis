aad8b4bb4d16523a074a4c2b2e3317ba4db578d1
Ubuntu <ubuntu@ip-172-31-47-194.us-west-2.compute.internal>
2015-04-12 02:46:58 +0000

    twilio and uber

--- apps/urls.py
+++ apps/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import patterns, url , include
 from rest_framework import routers
 from apps import views
 
+router = routers.DefaultRouter()
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^appointment/', api.appointment, name='appointment'),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^appointment/', views.appointment, name='appointment'),
+    url(r'^sms/', views.sms, name='sms'),
+    url(r'^uber_callback/', views.uber_callback, name='uber_callback')
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,7 @@
 import json
-
+import requests
+from django.http import HttpResponse
+from twilio.rest import TwilioRestClient
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.template import RequestContext, loader , Context
@@ -22,4 +24,32 @@ from rest_framework.decorators import detail_route, list_route
 def appointment(request):
 	return Response(status=status.HTTP_201_CREATED)
 
-		
+
+@api_view(['GET', 'POST'])
+def sms(request):
+	account_sid = "AC3a1890bb443c5abe1a07624e0c5624b2"
+	auth_token  = "59da12d6e084150099a80e398acc29a3"
+	client = TwilioRestClient(account_sid, auth_token)
+	 
+	message = client.messages.create(body="Jenny please?! I love you <3",
+	    to="+15519987206",    # Replace with your phone number
+	    from_="+16462333216") # Replace with your Twilio number
+	#print message.sid
+	return HttpResponse(status=201)
+
+
+@api_view(['GET', 'POST'])
+def uber_callback(request):
+	parameters = {
+		'redirect_uri': 'INSERT_ROUTE_TO_STEP_TWO',
+	    	'code': request.GET.get('code', ''),
+	    	# 'code': 'El34AvVt6Ismf688HJZ0ascNGXAWoI',
+		'grant_type': 'authorization_code',
+	}
+
+	response = requests.post('https://login.uber.com/oauth/token',auth=('oQ3VN3DLkVAETHYFwPLnw0JbgHwwxzro','FXt5qBewJCzYC642XVa0APR6NoShI0ahlB3QyTcD',),data=parameters,)
+	print request.GET.get('code', '')
+	print response
+	access_token = response.json().get('access_token')
+	return HttpResponse(access_token, content_type="application/json")
+	#return HttpResponse(status=404)
