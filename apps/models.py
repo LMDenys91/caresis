b001cce0357fdc51d2603f96027d5173c92e7469
Lucas Denys <lmd238@cornell.edu>
2015-04-11 21:52:27 -0400

    address

--- apps/models.py
+++ apps/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django import forms
 
+class Address(models.Model):
+	id = models.AutoField(primary_key=True)
+	address1 = models.CharField(max_length=200)
+	address2 = models.CharField(max_length=200)
+	city = models.CharField(max_length=200)
+	state = models.CharField(max_length=200)
+	zip_code = models.CharField(max_length=200)
+
 class Patient(models.Model):
 	id = models.AutoField(primary_key=True)
 	firstName = models.CharField(max_length=200)
 	lastName = models.CharField(max_length=200)
-	address = models.CharField(max_length=200)
+	address = models.ForeignKey(Address)
 	phone = forms.RegexField(regex=r'^\+?1?\d{9,15}$', 
                                 error_message = ("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
 	uber_token = models.CharField(max_length=200)
@@ -17,7 +25,7 @@ class Advocate(models.Model):
 	id = models.AutoField(primary_key=True)
 	firstName = models.CharField(max_length=200)
 	lastName = models.CharField(max_length=200)
-	address = models.CharField(max_length=200)
+	address = models.ForeignKey(Address)
 	phone = forms.RegexField(regex=r'^\+?1?\d{9,15}$', 
                                 error_message = ("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
 	uber_token = models.CharField(max_length=200)
@@ -32,8 +40,7 @@ class Appointment(models.Model):
 	advocate = models.ForeignKey(Advocate)
  	title = models.CharField(max_length=200)
  	description = models.CharField(max_length=200)
- 	latitude = models.FloatField()
- 	longitude = models.FloatField()
+ 	address = models.ForeignKey(Address)
  	date = models.DateTimeField('Time of appointment')
 
  	def __str__(self):
